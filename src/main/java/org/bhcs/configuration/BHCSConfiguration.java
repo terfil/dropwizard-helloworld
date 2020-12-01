8f4cb61b0c0dabd4949fb1afa032f169c9dbc71b
weizhuang <terfil@gmail.com>
2020-12-01 17:58:05 -0500

    init commit

---
+++ src/main/java/org/bhcs/configuration/BHCSConfiguration.java
@@ -0,0 +1,34 @@
+package org.bhcs.configuration;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class BHCSConfiguration extends Configuration {
+    @NotNull
+    @Valid
+    private DataSourceFactory dataSourceFactory = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
+    }
+
+    @NotEmpty
+    private String defaultName;
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+
+}
